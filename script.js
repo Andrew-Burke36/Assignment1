d4fbff3e452ff4cae5ee9e278ba0ddbcda536edd
andrew_burke <s10266774@connect.np.edu.sg>
2024-11-28 09:42:33 +0800

    commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const navBar = document.querySelector("div .sidebar"),
+menuBtn1 = document.querySelector(".nav_bar .button"),
+menuBtn2 = document.querySelector(".sidebar .sidebar_button"),
+overlay = document.querySelector(".overlay");
+
+menuBtn1.addEventListener("click", () => {
+        navBar.classList.toggle("active")
+});
+
+menuBtn2.addEventListener("click", () => {
+        navBar.classList.toggle("active")
+});
+
+overlay.addEventListener("click", () => {
+        navBar.classList.toggle("active")
+});
